ebb5ed99969209ec4b3f761d609ba3bb52494055
Josh Talley <jtdev84@gmail.com>
2022-06-07 20:32:49 +0100

    Add ToDoList & ToDo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import ToDoList from './ToDoList'
 
 function App() {
+  const [todos, setTodos] = useState(['To do 1', 'To do 2'])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ToDoList todos={todos} />
+      <input type="text" />
+      <button>Add To Do</button>
+      <button>Clear List</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
